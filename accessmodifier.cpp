b4378b44b6958d053bd73e6b7090163c0cb8b249
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-06 10:12:14 -0700

    A Basic C++  introduction to OOP ACCESS-MODIFIER

---
+++ accessmodifier.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class Employee
+{
+public:
+	string Name;
+	string Company;
+	int Age;
+	
+	 
+
+};
+
+int main()
+{
+	Employee employee1;
+    return 0;
+}
