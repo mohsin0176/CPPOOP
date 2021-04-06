18b81191a8dfa4cc45467add166ba8fe4048052a
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-06 10:29:58 -0700

    A Basic C++  introduction to OOP ENCAPSULATION

--- encapsulation.cpp
+++ encapsulation.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
 using namespace std;
+
 class Employee
 {
-public:
+private:
 	string Name;
 	string Company;
 	int Age;
+public:
+	void setName(string name)
+	{
+		return name;
+	}
+	void setCompany(string company)
+	{
+		Company=company;
+	}
+	string getCompany()
+	{
+		return Company;
+	}
+	void setAge(int age)
+	{
+		if (age>=18)
+		{
+			Age=age;
+		}
+	}
+	int getAge()
+	{
+		return Age;
+	}
 	
 	void IntroduceYourself()
 	{
@@ -28,5 +53,7 @@ int main()
 	employee1.IntroduceYourself();
 	Employee employee2=Employee("mohsin","YT-Code",45);
 	employee2.IntroduceYourself();
+	employee1.setAge(39);
+	cout<<employee1.getName()<<"is"<<employee1.getAge()<<"Year";
 	return 0;
 }
